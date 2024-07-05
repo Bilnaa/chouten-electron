476c3da4351eb3f66dc395b7c640aeafe6b04836
Bilnaa <noufele.mehdi@hotmail.fr>
2024-07-05 19:54:48 +0200

    chore: Update icon path in createWindow function

--- electron/main/index.ts
+++ electron/main/index.ts
@@ -45,7 +45,7 @@ const indexHtml = path.join(RENDERER_DIST, 'index.html')
 async function createWindow() {
   win = new BrowserWindow({
     title: 'Main window',
-    icon: path.join(process.env.VITE_PUBLIC, 'favicon.ico'),
+    icon: path.join(process.env.VITE_PUBLIC, 'chouten.png'),
     webPreferences: {
       preload,
       // Warning: Enable nodeIntegration and disable contextIsolation is not secure in production
