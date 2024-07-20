bc8e5043a0bb155430202fb50eafed0e1c29ff25
Nabil <noufnouf703@gmail.com>
2024-07-20 04:02:10 +0200

    feat: Add VideoPlayer component for streaming videos

--- src/routes.ts
+++ src/routes.ts
@@ -7,6 +7,7 @@ import Home from './components/Home.vue';
 import ComingSoon from './components/ComingSoon.vue';
 import Repo from './components/Repo.vue';
 import Info from './components/Info.vue';
+import VideoPlayer from './components/player/VideoPlayer.vue';
 
 
 export default createRouter({
@@ -32,6 +33,11 @@ export default createRouter({
         path:'/infos',
         component: Info,
         props : route => ({ url: route.query.url })
+      },
+      {
+        path : '/streams',
+        component:  VideoPlayer,
+        props : route => ({ episodeId: route.query.episodeId, episodeTitle: route.query.episodeTitle, title : route.query.title})
       }
      ]
 });
