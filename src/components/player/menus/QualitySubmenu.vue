df97dadea92191a14307a6fcfc9105cc009aa5a3
Nabil <noufnouf703@gmail.com>
2024-07-20 05:49:00 +0200

    refactor: Update QualitySubmenu.vue to improve code readability and maintainability + now it works lol

--- src/components/player/menus/QualitySubmenu.vue
+++ src/components/player/menus/QualitySubmenu.vue
@@ -1,38 +1,32 @@
 <script setup lang="ts">
-import Menu from './Menu.vue';
 import MenuRadio from './MenuRadio.vue';
 import Submenu from './Submenu.vue';
 </script>
 
 <template>
-  <Submenu label="Quality">
-    <template #icon>
-      <media-icon class="icon" type="settings-menu" />
-    </template>
-    <template #content>
-        <template>
-            <media-quality-radio-group>
-      <template>
-        <media-radio>
-          <span data-part="label"></span>
-          <span data-part="bitrate"></span>
-        </media-radio>
-      </template>
-    </media-quality-radio-group>
+    <Submenu label="Quality">
+        <template #icon>
+            <media-icon class="icon" type="settings-menu" />
         </template>
-    </template>
-  </Submenu>
+        <template #content>
+            <media-quality-radio-group class="radio-group">
+                <template>
+                    <MenuRadio />
+                </template>
+            </media-quality-radio-group>
+        </template>
+    </Submenu>
 </template>
 
 <style scoped>
 .icon {
-  width: 22px;
-  height: 22px;
+    width: 22px;
+    height: 22px;
 }
 
 .radio-group {
-  width: 100%;
-  display: flex;
-  flex-direction: column;
+    width: 100%;
+    display: flex;
+    flex-direction: column;
 }
 </style>
