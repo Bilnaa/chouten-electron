3bbd6f54dde7f008e737065226d98067017f6852
Nabil <noufnouf703@gmail.com>
2024-08-03 02:09:00 +0200

    chore: Update font-family to 'Poppins' in App.vue and style.css

--- src/App.vue
+++ src/App.vue
@@ -138,7 +138,7 @@ onMounted(() => {
 <style>
 body {
   margin: 0;
-  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  font-family: 'Poppins', sans-serif;
   overflow: hidden;
 }
 
